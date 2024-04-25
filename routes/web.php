b23348538494efc8dbcffb680fca2ea8fe18ea32
MD. Muhiminul Haque <apon2041@gmail.com>
2024-04-25 09:15:43 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,27 +22,24 @@ use App\Http\Controllers\SettingsController;
 
 
 Auth::routes(['register' => false]);
-
-// Routes for the Message model (resource routes first)
-//Route::resource('messages', MessageController::class);
 Route::get('/', [MessageController::class, 'index'])->name('home');
-Route::get('/prompt', [MessageController::class, 'prompt'])->name('prompt');
-Route::get('/get-count', [MessageController::class, 'getCountData'])->name('get-count');
-//AI
-Route::post('/ai', [AiGenerate::class, 'generate'])->name('ai');
+//Ajax routes
 
+Route::post('/ai', [AiGenerate::class, 'generate'])->name('ai'); //AI
+Route::get('/get-count', [MessageController::class, 'getCountData'])->name('get-count');
 //Message Actions
 Route::post('/replymonitor', [ReplyController::class, 'getMessage']); //details for the replied Message]
-Route::post('/release', [ReplyController::class, 'release']); //details for the replied Message]
-
 Route::post('/message', [MessageController::class, 'getMessage']); //details for the message]
+
+Route::post('/release', [ReplyController::class, 'release']); //details for the replied Message]
 Route::post('/redirect', [MessageController::class, 'redirect_send'])->name('redirect'); //Redirect the message
 Route::post('/reply', [MessageController::class, 'reply_send'])->name('reply'); //Redirect the message
 Route::post('/modifi-labels', [MessageController::class, 'modifiLabels'])->name('labels.modify'); //Redirect the message
 
 Route::post('/multiple-action', [MessageController::class, 'multipleAction']); //Redirect the message
+//Delete All
 Route::post('/delete-all', [MessageController::class, 'deleteAll']); //Redirect the message
-
+//Single Actions
 Route::get('/message/{id}/info', [MessageController::class, 'info'])->name('info');
 Route::get('/message/{id}/spam', [MessageController::class, 'makeSpam'])->name('message.spam');
 Route::get('/message/{id}/notspam', [MessageController::class, 'notSpam'])->name('message.notspam');
@@ -51,16 +48,33 @@ Route::get('/message/{id}/notlocal', [MessageController::class, 'notLocal'])->na
 Route::get('/message/{id}/redirect', [MessageController::class, 'redirect'])->name('message.redirect');
 Route::get('/message/{id}/delete', [MessageController::class, 'destroy'])->name('message.delete');
 Route::get('/message/{id}/reply', [MessageController::class, 'reply'])->name('message.reply');
+//Ends Ajax Routes
+
+Route::group(['prefix' => 'edesk'], function () {
+    // Routes for the Message model (resource routes first)
+    Route::get('/', [MessageController::class, 'index'])->name('home');
+    Route::get('/prompt', [MessageController::class, 'prompt'])->name('prompt');
+
+    Route::get('/messages/{status?}', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('/replies/{status?}', [ReplyController::class, 'index'])->name('messages.replies');
+    // Catch-all route for the root URL
+});
 
 
-Route::get('/messages/{status?}', [MessageController::class, 'index'])->name('messages.index');
-Route::get('/replies/{status?}', [ReplyController::class, 'index'])->name('messages.replies');
-// Catch-all route for the root URL
-Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
-Route::get('/auth-logout', [SettingsController::class, 'AuthLogout']);
-Route::post('/settings/update', [SettingsController::class, 'UpdateSettings']);
+Route::group(['prefix' => 'gdesk'], function () {
+    Route::get('/', [SettingsController::class, 'index'])->name("gdesk");
+});
 
-// Routes for the Domain model
-Route::resource('domains', DomainController::class);
+Route::group(['prefix' => 'settings'], function () {
+    Route::get('/general', [SettingsController::class, 'index'])->name('general');
+    Route::get('/auth-logout', [SettingsController::class, 'AuthLogout']);
+
+    Route::get('/accounts', [SettingsController::class, 'index'])->name('accounts.index');
+
+    Route::post('/settings/update', [SettingsController::class, 'UpdateSettings']);
+    // Routes for the Domain model
+    Route::resource('domains', DomainController::class);
+});
+
 // Routes for the Setting model
 //Route::resource('settings', SettingController::class);
