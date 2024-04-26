d11c511cbf9eb2f64bda27b399b868520c8fecdb
MD. Muhiminul Haque <apon2041@gmail.com>
2024-04-26 09:20:06 +0600

    Create 2024_04_25_084748_create_senders_table.php

---
+++ database/migrations/2024_04_25_084748_create_senders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('senders', function (Blueprint $table) {
+            $table->id();
+            $table->string('email_address')->unique()->required();
+            $table->boolean('auth_login_type')->default(false);
+            $table->json('smtp_options')->nullable();
+            $table->json('imap_options')->nullable();
+            $table->json('auth_token')->nullable();
+            $table->json('other_options')->nullable();
+            $table->unsignedInteger('daily_limit')->default(0);
+            $table->unsignedInteger('daily_send_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('senders');
+    }
+};
