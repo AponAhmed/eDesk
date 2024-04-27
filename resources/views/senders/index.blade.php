acb9a41a5cd2747a1056ce3476dce46b69ac681d
MD. Muhiminul Haque <apon2041@gmail.com>
2024-04-27 18:29:38 +0600

    Update index.blade.php

--- resources/views/senders/index.blade.php
+++ resources/views/senders/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="dataContainer px-4 w-full">
         <div class="flex py-2 border-b border-solid border-slate-100 mb-3 items-center">
-            <h1 class="font-thin text-xl">List of Senders</h1>
+            <h1 class="font-thin text-xl px-2">List of Senders</h1>
             <a href="{{ route('senders.create') }}"
-                class="popup create-button px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm ml-4">New
+                class="popup create-button px-4 py-1 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm ml-4">New
                 Sender</a>
         </div>
         <div class="table-wraper overflow-x-auto bg-white p-4 rounded-md border border-gray-200">
@@ -14,7 +14,7 @@
                     <tr>
                         <th class="hidden">ID</th>
                         <th>Email Address</th>
-                        <th>Auth Login Type</th>
+                        <th>Quota Left</th>
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -22,8 +22,8 @@
                     @foreach ($senders as $sender)
                         <tr>
                             <td class="hidden">{{ $sender->id }}</td>
-                            <td>{{ $sender->email_address }}</td>
-                            <td>{{ $sender->auth_login_type ? 'Yes' : 'No' }}</td>
+                            <td>{{ $sender->email_address }} {{ $sender->auth_login_type == 1 ? '(API)' : '(SMTP)' }}</td>
+                            <td>{{ $sender->daily_limit - $sender->daily_send_count }}</td>
                             <td>
                                 <div class="flex sep">
                                     <a class="popup" href="{{ route('senders.edit', $sender->id) }}">Edit</a>
