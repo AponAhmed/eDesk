36ad2f5977eb00766893d2000c507087e6954837
MD. Muhiminul Haque <apon2041@gmail.com>
2024-05-02 17:49:02 +0600

    Create 2024_05_02_034737_create_g_messages_table.php

---
+++ database/migrations/2024_05_02_034737_create_g_messages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('g_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 99);
+            $table->string('email', 256);
+            $table->string('subject', 256)->nullable();
+            $table->longText('message')->nullable();
+            $table->foreignId('sender_id')->constrained('senders');
+            $table->json('header')->nullable(); //
+            $table->json('labels')->default("[inbox,unread]");
+            $table->integer('reminder')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('g_messages');
+    }
+};
