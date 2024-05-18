9d9db7486cc7e78c25d77b29f837682c5bb25924
MD. Muhiminul Haque <apon2041@gmail.com>
2024-05-18 18:11:22 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CronController;
 use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/send', [ApiController::class, 'store'])->withoutMiddleware(['auth']);
 //Mobile Application
 Route::get('/check-new/{id?}', [ApiController::class, 'check4New']);
+
+Route::get('/get-message', [CronController::class, 'get']);
 
 // Route::middleware('auth:sanctum')->group(function () {
 //     // Protected routes requiring authentication
