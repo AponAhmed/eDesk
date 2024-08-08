0553b491d8b9126242cf3c31178d6d35ddaa41cd
MD. Muhiminul Haque <apon2041@gmail.com>
2024-08-08 16:12:59 +0600

    Update show.blade.php

--- resources/views/domains/show.blade.php
+++ resources/views/domains/show.blade.php
@@ -1,15 +1,18 @@
-<h1 class="text-lg mb-4 border-b border-solid border-slate-200 py-2">Domain Details</h1>
-<p><strong>ID:</strong> {{ $domain->id }}</p>
-<p><strong>URL:</strong> {{ $domain->url }}</p>
-<p><strong>Key:</strong> {{ $domain->key }}</p>
-<p><strong>Messages:</strong> {{ $domain->messages->count() }}</p>
+<h1 class="text-lg mb-4 border-b border-solid border-slate-200 py-2 dark:border-slate-700 dark:text-gray-300">Domain Details</h1>
+
+<p class="dark:text-gray-200"><strong>ID:</strong> {{ $domain->id }}</p>
+<p class="dark:text-gray-200"><strong>URL:</strong> {{ $domain->url }}</p>
+<p class="dark:text-gray-200"><strong>Key:</strong> {{ $domain->key }}</p>
+<p class="dark:text-gray-200"><strong>Messages:</strong> {{ $domain->messages->count() }}</p>
+
 <br>
-<hr>
-<div class="flex sep">
-    <a href="{{ route('domains.edit', $domain->id) }}" class="popup ml-0">Edit Domain</a>
+<hr class="dark:border-gray-600">
+
+<div class="flex sep dark:bg-gray-800 dark:text-gray-200">
+    <a href="{{ route('domains.edit', $domain->id) }}" class="popup ml-0 dark:text-cyan-400">Edit Domain</a>
     <form method="POST" action="{{ route('domains.destroy', $domain->id) }}">
         @csrf
         @method('DELETE')
-        <button type="submit">Delete Domain</button>
+        <button type="submit" class="text-red-500 hover:text-red-700 dark:text-red-400 dark:hover:text-red-300">Delete Domain</button>
     </form>
 </div>
