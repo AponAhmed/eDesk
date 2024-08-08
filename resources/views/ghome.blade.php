737dc4aa48f2ca450c1b0a26e2646428c8aac572
MD. Muhiminul Haque <apon2041@gmail.com>
2024-08-08 16:13:01 +0600

    Update ghome.blade.php

--- resources/views/ghome.blade.php
+++ resources/views/ghome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="mails" class="flex flex-col h-full ox-h w-full bg-white">
-        <div class="list-wraper ox-h">
+    <div id="mails" class="flex flex-col h-full ox-h w-full bg-white  dark:bg-gray-900">
+        <div class="list-wraper ox-h dark:border-gray-900">
             <!-- Check if $messages is empty -->
             @if ($messages->isEmpty())
                 <div class="px-7 py-7" role="alert">
@@ -25,16 +25,19 @@
                                 </div>
                             </div>
                             <div class="name-wraper flex flex-col md:flex-row md:items-center ox-h w-full">
-                                <a href="javascript:void(0)" class="form-name md:px-2 text-base">{{ $message->name }}</a>
+                                <a href="javascript:void(0)"
+                                    class="form-name md:px-2 text-base text-gray-800 dark:text-gray-200" title="{{ $message->name }}">{{ $message->name }}</a>
                                 <div class="list-content ox-h">
                                     <div class="message-control absolute right-1 top-0 flex">
-                                        <div class="leading-none text-sm px-2 pb-[1px] font-mono bg-gray-100 text-gray-700 rounded-full border border-solid border-gray-200"
-                                            title="Messaged To {{ $message->sender->email_address}}">
-                                            @php echo str_replace(['https://','http://'],"",$message->sender->email_address) @endphp
+                                        <div class="leading-none text-sm px-2 pb-[1px] font-mono bg-gray-100 text-gray-600 dark:bg-gray-700 dark:text-gray-400 rounded-full border border-solid border-gray-200 dark:border-gray-800"
+                                            title="Messaged To {{ $message->sender->email_address }}">
+                                            @php echo str_replace(['https://','http://'], "", $message->sender->email_address) @endphp
                                         </div>
                                     </div>
-                                    <a href="javascript:void(0)" class="subject-line">{{ $message->subject }}</a>
-                                    <span class="snippet hidden md:block">{{ $message->snippet(150) }}</span>
+                                    <a href="javascript:void(0)"
+                                        class="subject-line text-gray-800 dark:text-gray-200">{{ $message->subject }}</a>
+                                    <span
+                                        class="snippet hidden md:block text-gray-600 dark:text-gray-400">{{ $message->snippet(150) }}</span>
                                 </div>
                             </div>
                         </div>
@@ -45,7 +48,7 @@
 
         <!-- Pagination Links -->
         <div
-            class="action-pagination flex items-center py-1 px-2 justify-between bg-gray-50 border border-t-0 border-solid border-gray-200">
+            class="action-pagination flex items-center py-1 px-2 justify-between bg-gray-50 dark:bg-gray-800 dark:border-gray-900 border border-t-0 border-solid border-gray-200">
             <div class="action-wrap flex px-2 items-center">
                 <div class="checkbox-wrapper-31 mr-2">
                     <input class="checkAll" type="checkbox">
@@ -58,19 +61,19 @@
                 <div class="dropdown relative inline-block text-left">
                     <div>
                         <button title="Action"
-                            class="dropdown-toggler rounded-full flex flex-col items-center justify-between h-7 w-7 p-2 bg-white border border-solid border-gray-300">
-                            <span class="bg-gray-700 h-[2px] w-[2px] rounded-full"></span>
-                            <span class="bg-gray-700 h-[2px] w-[2px] rounded-full"></span>
-                            <span class="bg-gray-700 h-[2px] w-[2px] rounded-full"></span>
+                            class="dropdown-toggler rounded-full flex flex-col items-center justify-between h-7 w-7 p-2 bg-white dark:bg-gray-800 border border-solid border-gray-300 dark:border-gray-600">
+                            <span class="bg-gray-700 dark:bg-gray-300 h-[2px] w-[2px] rounded-full"></span>
+                            <span class="bg-gray-700 dark:bg-gray-300 h-[2px] w-[2px] rounded-full"></span>
+                            <span class="bg-gray-700 dark:bg-gray-300 h-[2px] w-[2px] rounded-full"></span>
                         </button>
                     </div>
-                    <div class="dropdown-content absolute left-0 bottom-full z-10 mb-2 w-56  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    <div class="dropdown-content absolute left-0 bottom-full z-10 mb-2 w-56 rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10 focus:outline-none"
                         role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                         <div class="py-1" role="none">
                             @foreach ($actions as $indx => $action)
                                 <a href="javascript:void(0)" data-action-title="{{ $action->label }}"
                                     data-action="{{ $action->action }}"
-                                    class="multiple-action-trigger text-gray-700 block px-4 py-2 text-sm hover:bg-slate-100"
+                                    class="multiple-action-trigger text-gray-700 dark:text-gray-300 block px-4 py-2 text-sm hover:bg-slate-100 dark:hover:bg-gray-900"
                                     role="menuitem" tabindex="-1"
                                     id="menu-item-{{ $indx }}">{{ $action->label }}</a>
                             @endforeach
@@ -91,5 +94,5 @@
         </div>
 
     </div>
-    <div id="messageDetails" class="mail-details h-full bg-white"></div>
+    <div id="messageDetails" class="mail-details h-full bg-white dark:bg-gray-900"></div>
 @endsection
