0893af1a3149549648766d741f64c1e19913ac5e
MD. Muhiminul Haque <apon2041@gmail.com>
2024-08-08 16:12:47 +0600

    Update redirect.blade.php

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -1,11 +1,12 @@
 <form method="POST" action="{{ route(request()->segment(1) == 'gdesk' ? 'gredirect' : 'redirect') }}" class="ajx">
     @csrf
     <input type="hidden" name="message_id" value="{{ $id }}">
+
     <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="redirect_to">Redirect To</label>
+        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" for="redirect_to">Redirect To</label>
         <div class="relative">
             <select name="redirect_to"
-                class="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                class="block appearance-none w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 hover:border-gray-500 dark:hover:border-gray-600 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline dark:focus:border-blue-500 dark:text-gray-300">
                 @foreach ($emails as $email => $name)
                     <option value="{{ $name }}:{{ $email }}">
                         {{ $name }}&lt;{{ $email }}&gt;</option>
@@ -15,12 +16,12 @@
     </div>
 
     <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="redirect_to_custom">Custom Email Address</label>
+        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" for="redirect_to_custom">Custom Email Address</label>
         <input type="text" name="redirect_to_custom"
-            class="appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            class="appearance-none w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 hover:border-gray-500 dark:hover:border-gray-600 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline dark:focus:border-blue-500 dark:text-gray-300"
             placeholder="Email Address">
     </div>
 
     <button type="submit"
-        class="button mt-4 px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-md shadow-sm">Send</button>
+        class="button mt-4 px-4 py-2 font-semibold text-sm bg-cyan-500 dark:bg-cyan-700 text-white dark:text-gray-200 rounded-md shadow-sm">Send</button>
 </form>
